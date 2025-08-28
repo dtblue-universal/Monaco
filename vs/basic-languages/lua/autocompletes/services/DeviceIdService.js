3305b45cb709e8e30ae5c214d46754de17f1a58e
$ dtblue <94876822+dtblue-universal@users.noreply.github.com>
2025-08-28 10:25:56 -0500

    Add files via upload

---
+++ vs/basic-languages/lua/autocompletes/services/DeviceIdService.js
@@ -0,0 +1,1 @@
+define(['require','exports'],(require,exports)=>{Object.defineProperty(exports,'__esModule',{value:true});exports['autocompletes']={"Method":{"GetDeviceId()":{"insertText":"GetDeviceId()","documentation":{"value":"[View documentation](https://create.roblox.com/docs/reference/engine/classes/DeviceIdService#GetDeviceId)"}}},"Enum":{},"Field":{},"Event":{}};});
